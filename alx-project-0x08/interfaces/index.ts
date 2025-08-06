798435120fab8dd06f7c82e2df5c4a00ee430235
Mac <mac@okey101.local>
2025-08-06 17:41:24 +0100

    Modify application state to keep track of basic state

---
+++ alx-project-0x08/interfaces/index.ts
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+export interface ReactComponentProps {
+    children: React.ReactNode
+}
+
+export interface GeneratedImageProps {
+    imageUrl: string,
+    prompt: string,
+    width?: number,
+    height?: number,
+    action: (imagePath: string) => void,
+}
+
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">;
